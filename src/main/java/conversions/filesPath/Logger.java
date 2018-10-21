2d67674507d484a01e12a8658162e70f50f12fe3
Maciej Krzesinski <maciej.krzesinski@gmail.com>
2018-10-21 19:47:44 +0200

    Logger added.

---
+++ src/main/java/conversions/filesPath/Logger.java
@@ -0,0 +1,13 @@
+package conversions.filesPath;
+
+import conversions.convToJSON.user.ConvUser;
+import run.runConvToJSON.Main;
+
+public class Logger {
+
+    final static org.apache.log4j.Logger logger = org.apache.log4j.Logger.getLogger(ConvUser.class);
+
+    public static void main(String[] args) {
+
+    }
+}
