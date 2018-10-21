04d9e47e1c1acdfcfe13aa39242de6293104ce7a
Maciej Krzesinski <maciej.krzesinski@gmail.com>
2018-10-21 22:21:00 +0200

    Logger works fine.

--- src/main/java/conversions/support/Logger.java
+++
@@ -1,9 +0,0 @@
-package conversions.support;
-
-public class Logger {
-    final static org.apache.log4j.Logger logger = org.apache.log4j.Logger.getLogger(Logger.class);
-
-    public static org.apache.log4j.Logger getLogger() {
-        return logger;
-    }
-}
